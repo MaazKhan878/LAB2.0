dc0f4146c2fe8337b1e8983cb947cb615d57a36d
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-18 22:20:50 +0500

    dollar.c prompt to user and take 10% exchange fee then dollar to pound and dollar to yen

--- dollar.c
+++ dollar.c
@@ -2,5 +2,11 @@
 int main(){
 float dollar, exchangeFee, dollarPerPound, totalAmountDollar;
 int dollarPerYen;
+printf("Enter the amounrt in Dollar:");
+scanf("%f",&dollar);
+exchangeFee=dollar*0.1;
+totalAmountDollar=dollar - exchangeFee;
+dollarPerPound = (totalAmountDollar/2) * 0.79;
+dollarPerYen = (totalAmountDollar/2) * 127.65;
 return 0;
 }
