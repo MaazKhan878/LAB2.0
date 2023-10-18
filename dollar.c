56e60294aaa3208ca0d050983ce1335d6670cacb
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-18 22:23:42 +0500

    output show

--- dollar.c
+++ dollar.c
@@ -8,5 +8,7 @@ exchangeFee=dollar*0.1;
 totalAmountDollar=dollar - exchangeFee;
 dollarPerPound = (totalAmountDollar/2) * 0.79;
 dollarPerYen = (totalAmountDollar/2) * 127.65;
+printf("Half of the Dollar exchange to %g GBP\n",dollarPerPound);
+printf("Hal of the Dollar exchange to %d jpy\n",dollarPerYen);
 return 0;
 }
